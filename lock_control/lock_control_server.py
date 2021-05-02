16a9452ea60e2e3320088f6f42700ac534002114
wcairnx <willcairncross@gmail.com>
2021-05-02 13:16:02 -0400

    syntax err fix

--- lock_control/lock_control_server.py
+++ lock_control/lock_control_server.py
@@ -26,7 +26,8 @@ class lock_control_server(object):
             return 
         cmd = self.__sock.recv_string()
         attr = getattr(self.lc, cmd)
-        attr_type = print(type(attr))
+        attr_type = type(attr)
+        print(attr_type)
         self.__sock.send_string(str(attr_type))
 
 def main():
