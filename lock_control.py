aa07b49047655751b8db7ca9d0ac50a2d28ee94a
wcairnx <willcairncross@gmail.com>
2021-05-06 17:47:45 -0400

    cannot set single ao with an array

--- lock_control.py
+++ lock_control.py
@@ -100,7 +100,8 @@ class lock_control:
                 self.__owner.daq.ao.run()
             else:
                 self.__owner.daq.ao.stop()
-                self.__owner.daq.ao.set_single(channel=[0,1],value=[2.5,2.5])
+                self.__owner.daq.ao.set_single(channel=0,value=2.5)
+                self.__owner.daq.ao.set_single(channel=1,value=2.5)
             
             
     class _wm:
