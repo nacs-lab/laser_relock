5ac646652f0f382c38c49a3e3a8489bbdf6d7b12
nacslab <niharvardlab@gmail.com>
2021-05-07 13:55:40 -0400

    lower duty cycle trigger signal

--- lock_control.py
+++ lock_control.py
@@ -104,7 +104,7 @@ class lock_control:
                 data1 = amp1 * signal.sawtooth(2 * np.pi * freq * t,0.5) + amp1
             else:
                 data1 = 0.0 * t
-            data0 = 2.5 * (signal.square(2 * np.pi * freq * (t),0.5) + 1.0)
+            data0 = 2.5 * (signal.square(2 * np.pi * freq * t,0.1) + 1.0)
                 
             data = np.concatenate((data0[:,None],data1[:,None]),axis=1)
             data = np.reshape(data,(2*len(t),))
