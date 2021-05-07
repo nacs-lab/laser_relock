e013531aa5e4fdae070f6b82c1176d987b7ed351
wcairnx <willcairncross@gmail.com>
2021-05-07 19:27:29 -0400

    added keithley control class

---
+++ keithley/keithley.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+import usbtmc
+
+VENDOR = 1510
+PRODUCT = 8704
+
+class keithley:
+    def __init__(self):
+        self.vendor = VENDOR
+        self.product = PRODUCT
+        self.connect()
+        self.VoltageLimit = 10.0
+        self.CurrentLimit = 1.0
+        self.Voltage = self.getVoltage()
+        self.Current = self.getCurrent()
+        
+    def connect(self):
+        self.inst = usbtmc.Instrument(VENDOR,PRODUCT)
+        self.inst.open()
+        self.inst.write('SYST:REM')
+        
+    def close(self):
+        self.inst.close()
+    
+    def getCurrent(self):
+        self.Current = float(self.inst.ask('SOUR:CURR:LEV?'))
+        return self.Current
+        
+    def getVoltage(self):
+        self.Voltage = float(self.inst.ask('SOUR:VOLT:LEV?'))
+        return self.Voltage
+    
+    def measureVoltage(self):
+        res = float(self.inst.ask('MEAS:VOLT?'))
+        return res
+    
+    def measureCurrent(self):
+        res = float(self.inst.ask('MEAS:CURR?'))
+        return res
+        
+    def getIDN(self):
+        return self.inst.ask("*IDN?")
+    
+    def setCurrent(self,current):
+        if (current > self.CurrentLimit) or (current < 0.0):
+            raise Exception("current out of range")
+        self.inst.write('SOUR:CURR:LEV %.3f\n' % current)
+        
+    def setVoltage(self,voltage):
+        if (abs(voltage) > self.VoltageLimit) or abs(voltage) > 30.0:
+            raise Exception("voltage out of range")
+        self.inst.write('SOUR:VOLT:LEV %.3f\n' % voltage)
+        
+    def setOutput(self,output):
+        self.inst.write('SOUR:OUTP:STAT %.3f\n' % output)
+
